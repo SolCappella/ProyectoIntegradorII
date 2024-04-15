d059647f7680bfe6adda4ca96ee67ad4338facda
maisaccani <msaccani@udesa.edu.ar>
2024-04-15 12:32:34 -0300

    titulos

--- ProyectoIntegradorII/controllers/mainController.js
+++ ProyectoIntegradorII/controllers/mainController.js
@@ -5,7 +5,7 @@ const mainController = {
         res.render('index');
     },
     'login': function mostrarFormLogin(req, res) {
-        res.render('login')
+        res.render('login', {title:'Iniciar sesión'})
     },
     'register': function (req, res) {
         res.render('register', { title: "Registrate" })
