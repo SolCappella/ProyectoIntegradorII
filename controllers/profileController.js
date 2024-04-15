899c50d327c000c47462cd0eac5951a233be64f2
TomyWes <tomyweskamp@github.com>
2024-04-15 17:50:29 -0300

    db y profile

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,12 +1,20 @@
-const db=require('../db/data');
+const db = require('../db/data');
 
 const profileController={
     'profile':function (req,res) {
-        res.render('profile');
+        let user = db.usuario; 
+
+        let stats = {
+            productos: 8,
+            seguidores: 200,
+            comentarios: 300,
+        }
+
+        res.render('profile', {user, stats});
     },
     'edit': function (req, res) {
         res.render('profile-edit');
     },
 }
 
-module.exports= profileController;
+module.exports = profileController;
