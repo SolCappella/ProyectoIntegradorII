18a0079b0f7558a84ea0ac2a906ca6f04c249561
TomyWes <tomyweskamp@github.com>
2024-06-08 16:25:17 -0300

    modulo comment y sus relaciones

---
+++ db/config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "development": {
+      "username": "root",
+      "password": "root",
+      "database": "proyectoprogra",
+      "host": "127.0.0.1",
+      "dialect": "mysql",
+      "port": "8889" 
+    }
+  }
+
