7a98df4d68763b8d6a493e2e48c95d5af76df9c0
Sol Cappella <solcappella@Sols-MacBook-Pro-2.local>
2024-06-10 12:00:21 -0300

    modelo user

---
+++ db/models/user.js
@@ -0,0 +1,64 @@
+module.exports = function(sequelize, dataTypes) {
+    let alias = 'user' ;
+
+    let cols = {
+        id : {
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER
+        },
+        email : {
+            type:dataTypes.STRING,
+            allowNull:false
+        },
+        contraseña : {
+            type:dataTypes.STRING,
+            allowNull:false
+        },
+        fecha : {
+            type: dataTypes.DATE,
+            allowNull: true,
+        },
+        dni : {
+            type: dataTypes.INTEGER,
+            allowNull: true
+        },
+        foto : {
+            type:dataTypes.STRING,
+            allowNull:true
+        },
+        created_at: {
+            type: dataTypes.DATE,
+            allowNull: false,
+        },
+        updated_at: {
+            type: dataTypes.DATE,
+            allowNull: false,
+        },
+        deleted_at:{
+            type:dataTypes.DATE,
+            allowNull:true
+        }
+    };
+    let config = {
+        tableName: "usuarios",
+        timestamps: true,        
+        underscored: true,       
+    };
+
+    const User = sequelize.define(alias, cols, config);
+
+    User.associate = function (models) {
+        User.hasMany(models.Product, {
+            as: "product", 
+            foreignKey: "usuario_id",
+        });
+        User.hasMany(models.Comment, {
+            as: "comment", 
+            foreignKey: "comment_id",
+        });
+    
+    }
+    return User;
+    
+}
